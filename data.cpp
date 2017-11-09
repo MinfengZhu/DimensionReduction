d4f9ef171171741eb8be79e572d146eed46b9454
twilightsnow <zhuminfeng@live.cn>
2017-11-09 11:00:44 +0800

    .

--- data.cpp
+++ data.cpp
@@ -13,45 +13,43 @@
 
 data::data()
 {
-	vec = NULL;
+    vec = NULL;
 }
 
 void data::clean_data()
 {
-	if (vec) { delete[] vec; vec = NULL; }
+    if (vec) { delete[] vec; vec = NULL; }
 }
 
 real* data::load_from_file(char *infile)
 {
-	clean_data();
-	FILE *fin = fopen(infile, "rb");
-	if (fin == NULL)
-	{
-		printf("\nFile not found!\n");
-		return 0;
-	}
+    clean_data();
+    FILE *fin = fopen(infile, "rb");
+    if (fin == NULL)
+    {
+        printf("\nFile not found!\n");
+        return 0;
+    }
     cout<<"[DATA] "<<"Read from "<<infile<<endl;
-	auto ret = fscanf(fin, "%lld%lld", &n_vertices, &n_dim);
+    auto ret = fscanf(fin, "%lld%lld", &n_vertices, &n_dim);
     if (ret == EOF){
-		printf("Reading error");
-		return 0;
-	}
-	vec = new float[n_vertices * n_dim];
+        printf("Reading error");
+        return 0;
+    }
+    vec = new float[n_vertices * n_dim];
     knn_vec = new vector<int>[n_vertices];
-	for (long long i = 0; i < n_vertices; ++i){
-		for (long long j = 0; j < n_dim; ++j){
+    for (long long i = 0; i < n_vertices; ++i){
+        for (long long j = 0; j < n_dim; ++j){
             auto ret = fscanf(fin, "%f", &vec[i * n_dim + j]);
-			if (ret == EOF){
-				printf("Reading error");
-				return 0;
-			}
-		}
-	}
-//    cout << "[TEST PRE ] " << vec[176] << endl;
-	fclose(fin);
-	show_data_info();
-	normalize();
-//    cout << "[TEST AFTER ] " << vec[176] << endl;
+            if (ret == EOF){
+                printf("Reading error");
+                return 0;
+            }
+        }
+    }
+    fclose(fin);
+    cout<<"[DATA] "<<"#vertices="<<n_vertices<<" "<<"#dim="<<n_dim<<endl;
+    normalize();
     return vec;
 }
 
@@ -62,41 +60,39 @@ long long data::get_vertice_number(){
 long long data::get_dim_number(){
     return n_dim;
 }
+
 real* data::getVec(){
-	return vec;
-}
-void data::show_data_info(){
-    cout<<"[DATA] "<<"#vertices="<<n_vertices<<" "<<"#dim="<<n_dim<<endl;
+    return vec;
 }
 
 void data::normalize() {
-	cout << "[DATA] " << "Normalizing ......";
-	real *mean = new real[n_dim];
-	for (long long i = 0; i < n_dim; ++i) mean[i] = 0;
-	for (long long i = 0, ll = 0; i < n_vertices; ++i, ll += n_dim)
-	{
-		for (long long j = 0; j < n_dim; ++j)
-			mean[j] += vec[ll + j];
-	}
-	for (long long j = 0; j < n_dim; ++j)
-		mean[j] /= n_vertices;
-	real mX = 0;
-	for (long long i = 0, ll = 0; i < n_vertices; ++i, ll += n_dim)
-	{
-		for (long long j = 0; j < n_dim; ++j)
-		{
-			vec[ll + j] -= mean[j];
-			if (fabs(vec[ll + j]) > mX)	mX = fabs(vec[ll + j]);
-		}
-	}
-	for (long long i = 0; i < n_vertices * n_dim; ++i)
-		vec[i] /= mX;
-	delete[] mean;
-	cout << " Done." << endl;
+    cout << "[DATA] " << "Normalizing ......";
+    real *mean = new real[n_dim];
+    for (long long i = 0; i < n_dim; ++i) mean[i] = 0;
+    for (long long i = 0, ll = 0; i < n_vertices; ++i, ll += n_dim)
+    {
+        for (long long j = 0; j < n_dim; ++j)
+            mean[j] += vec[ll + j];
+    }
+    for (long long j = 0; j < n_dim; ++j)
+        mean[j] /= n_vertices;
+    real mX = 0;
+    for (long long i = 0, ll = 0; i < n_vertices; ++i, ll += n_dim)
+    {
+        for (long long j = 0; j < n_dim; ++j)
+        {
+            vec[ll + j] -= mean[j];
+            if (fabs(vec[ll + j]) > mX)	mX = fabs(vec[ll + j]);
+        }
+    }
+    for (long long i = 0; i < n_vertices * n_dim; ++i)
+        vec[i] /= mX;
+    delete[] mean;
+    cout << " Done." << endl;
 }
 
 vector<int>* data::getKnnVec () {
-	return knn_vec;
+    return knn_vec;
 }
 
 #endif
